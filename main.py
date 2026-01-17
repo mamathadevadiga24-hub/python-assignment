e950a48784ff02e035cfeca7cd9f85919a584de9
mamathadevadiga24-hub <mamathadevadiga24@gmail.com>
2026-01-17 22:00:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+
+print("addition of a and b is:",a+b)
+print("subtraction of a and b is:",a-b)
+print("multiplication of a and b is:",a*b)
+print("division of a and b is:",a/b)
+
+"""TASK 2"""
+Name=input("Enter your name:")
+LastName=input("Enter your last name:")
+
+print("Hello",Name,LastName,"Welcome to python class")
